bc352a157e4ded0ace4c9ca35e0ac85ce6719e3b
Oscar Groppfeldt <oscar.groppfeldt@gmail.com>
2024-03-11 23:03:04 +0100

    First draft, lots hardcoded

---
+++ andy.py
@@ -0,0 +1,124 @@
+import discord
+from discord.ext import commands
+from discord.ext import tasks
+import asyncio
+
+import random
+
+import utils
+import datetime
+import os
+
+class ScheduleCog(commands.Cog):
+
+	def __init__(self, bot):
+		self.bot = bot
+		self.schedule_url = "https://esparven.se/none/Team/Index/165"
+		self.games = []
+		self.current_msg_ctx = None
+		self.bot_ctx = None
+		self.games = utils.get_schedule(self.schedule_url)
+		self.games.sort(key=lambda x: x[1])
+		self.message_pinned = False
+
+	@commands.Cog.listener()
+	async def on_ready(self):
+		print(f"Logged in as {self.bot.user} (ID:{self.bot.user.id})")
+		print("----------------------")
+
+	@commands.Cog.listener()
+	async def on_message(self, msg):
+		if msg.author == self.bot.user:
+			return
+		print(f"Message from {msg.author}: {msg.content}")
+
+
+	@commands.command()
+	async def start(self, ctx):
+		if self.bot_ctx is None:
+			self.bot_ctx = ctx
+		await ctx.channel.send(f"Bot init: {datetime.datetime.now()}")
+
+
+	@commands.command()
+	async def schedule(self, ctx):
+		str2send = f"___Kommande matcher___\n"
+		today = datetime.datetime.now()
+		for game in self.games:
+			if game[1] > today:
+				game_name = game[0]
+				game_datetime_str = game[1].strftime("%Y-%m-%d %H:%M")
+				str2send += f"{game_name:<25}{game_datetime_str:>20}\n"
+		
+		await ctx.channel.send(str2send)
+
+	@commands.command()
+	async def update_url(self, msg):
+		self.schedule_url = msg.content
+		self.games = utils.get_schedule(self.schedule_url)
+		self.games.sort(key=lambda x: x[1])
+	
+	@tasks.loop(hours=168)
+	async def update_schedule(self):
+		self.games = utils.get_schedule(self.schedule_url)
+		self.games.sort(key=lambda x: x[1])
+
+	@tasks.loop(hours=24)
+	async def check_schedule(self):
+		next_game = self.games[0]
+		if next_game[1] < datetime.datetime.now() + datetime.timedelta(days=5):
+			await self.send_game_info(next_game, self.bot_ctx)
+		
+	@tasks.loop(hours=24)
+	async def check_team(self):
+		next_game = self.games[0]
+		if next_game[1] < datetime.datetime.now() + datetime.timedelta(days=3):
+			await self.generate_team(self.bot_ctx)
+		
+	async def send_game_info(self, next_game, bot_ctx):
+		message = "___Match!___\n"
+		game_name = next_game[0]
+		game_datetime_str = next_game[1].strftime("%Y-%m-%d %H:%M")
+		message += f"{game_name:<30}{game_datetime_str:<25}\n"
+		emoji_thumb_up = '\N{THUMBS UP SIGN}'
+		emoji_thumb_down = '\N{THUMBS DOWN SIGN}'
+		message += f"\n\nReagera med {emoji_thumb_up} om ni vill lira"
+		self.current_msg_ctx = await bot_ctx.channel.send(message)
+		await self.current_msg_ctx.add_reaction(emoji_thumb_up)
+		await self.current_msg_ctx.add_reaction(emoji_thumb_down)
+		self.current_msg_ctx.pin()
+		self.message_pinned = True
+
+
+	async def generate_team(self, bot_ctx):
+		msg = await self.current_msg_ctx.fetch_message(self.current_msg_ctx.id)
+		self.current_msg_ctx.unpin()
+
+		available_players = []
+		reactions = msg.reactions
+		emoji_thumbs_up = '\N{THUMBS UP SIGN}'
+		for reaction in reactions:
+			if reaction.emoji == emoji_thumbs_up:
+				available_players = [user for user in reaction.users()]
+		
+		if len(available_players) < 5:
+			await bot_ctx.channel.send("Vi saknar {5 - len(available_players)} spelare")
+		else:
+			players = random.choice(available_players)
+			await bot_ctx.channel.send(f"{players[0].mention}, {players[1].mention}, {players[2].mention}, {players[3].mention} och {players[4].mention} lirar")
+		self.message_pinned = False
+		self.games = self.games[1:]
+
+async def main(intents):
+	bot = commands.Bot(command_prefix='!', intents=intents)
+	await bot.add_cog(ScheduleCog(bot))
+	return bot
+
+if __name__ == "__main__":
+	TOKEN = os.environ['ANDYTOKEN']
+	intents = discord.Intents.default()
+	intents.reactions = True
+	intents.messages = True
+	intents.message_content = True
+	bot = asyncio.run(main(intents))
+	bot.run(TOKEN)
---
+++ utils.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup as bs
+import datetime
+import requests
+
+
+
+def get_schedule(url):
+	response = requests.get(url)
+	html = bs(response.content, features='html5lib')
+	tables = html.findAll('table', attrs={'class': 'table table-ekorpen text-light'})
+	body_tags = tables[0].findAll('tbody')
+	tr_tags = body_tags[0].findAll('tr')
+	games = []
+	for tr_tag in tr_tags:
+		td_tags = tr_tag.findAll('td')
+		games.append((td_tags[0].text.strip(), datetime.datetime.strptime(td_tags[1].text.strip(), "%Y-%m-%d %H:%M")))
+
+	return games
